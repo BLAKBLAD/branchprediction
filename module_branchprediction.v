7ff7def6e411d97f5fbb800b26579b0f53a3b5a9
stringname <lmh200103@163.com>
2024-01-09 03:07:06 +0800

    1. 更改了taken的含义 2. 添加了输入信号isbranch 3. 双发射

--- module_branchprediction.v
+++ module_branchprediction.v
@@ -1,22 +1,35 @@
 `timescale 1ns / 1ps
 
-//taken 上一条指令预测的结果，1-是跳转指令且跳转成功
-//target 跳转地址
+//更新使能train_valid：1-更新，0-不更新
+//是/不是分支指令isbranch：1-是分支指令，0-不是分支指令
+//跳转结果taken：1-是分支指令且确实发生了跳转，0-是分支指令但是没有发生跳转
 
 module module_branchprediction(
     input clk,
     input rst,
-    input [6:0] opcode,
-    input [31:0] currentPC,
-    input update,
-    input [31:0] branchPC,
-    input [31:0] resultPC,
-    input taken,
-    output [31:0] target
+    input [31:0] PC0,
+    input [31:0] PC1,
+    input train_valid0,
+    input train_valid1,
+    input isbranch0,
+    input isbranch1,
+    input [31:0] address_branch0,
+    input [31:0] address_branch1,
+    input [31:0] address_result0,
+    input [31:0] address_result1,
+    input taken0,
+    input taken1,
+    output [31:0] target0,
+    output [31:0] target1
     );
     
     //调用module_gselect
-    module_gselect modulex(.clk(clk), .isbranch(isbranch), .update(update), 
-    .lastPC(lastPC), .currentPC(currentPC), .taken(taken), .target(target));
+    module_gselect modulex(.clk(clk), .rst(rst), .PC0(PC0), .PC1(PC1), .train_valid0(train_valid0), 
+    .train_valid1(train_valid1), .isbranch0(isbranch0), .isbranch1(isbranch1), 
+    .address_branch0(address_branch0), .address_branch1(address_branch1), 
+    .address_result0(address_result0), .address_result1(address_result1), 
+    .taken0(taken0), .taken1(taken1), 
+    .target0(target0), .target1(target1)
+    );
     
 endmodule
--- module_gselect.v
+++ module_gselect.v
@@ -3,29 +3,164 @@
 module module_gselect(
     input clk,
     input rst,
-    input [6:0] opcode,
-    input [31:0] currentPC,
-    input update,
-    input [31:0] branchPC,
-    input [31:0] resultPC,
-    input taken,
-    output target
+    input [31:0] PC0,
+    input [31:0] PC1,
+    input train_valid0,
+    input train_valid1,
+    input isbranch0,
+    input isbranch1,
+    input [31:0] address_branch0,
+    input [31:0] address_branch1,
+    input [31:0] address_result0,
+    input [31:0] address_result1,
+    input taken0,
+    input taken1,
+    output [31:0] target0,
+    output [31:0] target1
     );
     
-    wire output_module_isbranch;
-    wire [7:0] output_module_GHR;
-    wire output_module_PHT;
+    reg [7:0] GHR;
+    reg [1:0] PHT [255:0][255:0];//[1:0] PHT[2^widthofGHR-1:0][2^lowerbitsofPC-1:0]
+    reg taken_predict0, taken_predict1;
+    reg [63:0] BTB [1023:0];//direct mapping
+    reg valid [1023:0];
+    reg [31:0] nextPC0, nextPC1;
+    integer i, j;
     
-    module_isbranch module_0(.opcode(opcode), .isbranch(output_module_isbranch));
+    //update GHR
+    //在retire阶段更新GHR是比较滞后的，在预测后的下一个时钟应该立即根据PHT的预测结果做出一个预测性更新，retire阶段发现不对再改
+    //但是这么写的时序bug有点多，先搁置
+     always @(posedge clk) begin
+        if(rst)begin
+            GHR <= 0;
+        end
+        else begin
+            if((train_valid0 && isbranch0) && (train_valid0 && isbranch0))begin
+                GHR <= {GHR[5:0], taken0, taken1};
+            end
+            else if(train_valid0 && isbranch0)begin
+                GHR <= {GHR[6:0], taken0};
+            end
+            else if(train_valid1 && isbranch1)begin
+                GHR <= {GHR[6:0], taken1};
+            end
+            else begin
+                GHR <= GHR;
+            end
+        end
+    end
     
-    module_GHR module_1(.clk(clk), .rst(rst), .update(update), .GHR(output_module_GHR));
+    //update PHT and get taken_predict
+    always @(posedge clk) begin
+        if(rst)begin
+            for(i = 0; i < 256; i = i + 1)begin
+                for(j = 0; j < 256; j = j + 1)begin
+                    PHT[i][j] <= 0;
+                end
+            end
+        end
+        else begin
+            if(train_valid0)begin
+                if(!isbranch0)begin
+                    PHT[GHR][address_branch0[9:2]] <= 0;
+                    taken_predict0 <= 0;
+                end
+                else if(taken0)begin
+                    if(PHT[GHR][address_branch0[9:2]] != 2'b11)begin
+                        PHT[GHR][address_branch0[9:2]] <= PHT[GHR][address_branch0[9:2]] + 1;
+                        taken_predict0 <= PHT[GHR][address_branch0[9:2]][1] != 2'b00;
+                    end
+                end
+                else begin
+                    if(PHT[GHR][address_branch0[9:2]] != 2'b00)begin
+                        PHT[GHR][address_branch0[9:2]] <= PHT[GHR][address_branch0[9:2]] - 1;
+                        taken_predict0 <= PHT[GHR][address_branch0[9:2]][1] == 2'b11; 
+                    end
+                end
+            end
+            else begin
+                PHT[GHR][address_branch0[9:2]] <= PHT[GHR][address_branch0[9:2]];
+                taken_predict0 <= PHT[GHR][address_branch0[9:2]];
+            end
+            if(train_valid1)begin
+                if(!isbranch1)begin
+                    PHT[GHR][address_branch1[9:2]] <= 0;
+                    taken_predict1 <= 0;
+                end
+                else if(taken1)begin
+                    if(PHT[GHR][address_branch1[9:2]] != 2'b11)begin
+                        PHT[GHR][address_branch1[9:2]] <= PHT[GHR][address_branch1[9:2]] + 1;
+                        taken_predict1 <= PHT[GHR][address_branch1[9:2]][1] != 2'b00;
+                    end
+                end
+                else begin
+                    if(PHT[GHR][address_branch1[9:2]] != 2'b00)begin
+                        PHT[GHR][address_branch1[9:2]] <= PHT[GHR][address_branch1[9:2]] - 1;
+                        taken_predict1 <= PHT[GHR][address_branch1[9:2]][1] == 2'b11;
+                    end
+                end
+            end
+            else begin
+                PHT[GHR][address_branch1[9:2]] <= PHT[GHR][address_branch1[9:2]];
+                taken_predict1 <= PHT[GHR][address_branch1[9:2]];
+            end
+        end
+    end
     
-    module_setofPHT module_2(.clk(clk), .rst(rst), .update(update), .BHR(output_module_GHR), 
-    .branchPC_lower(branchPC[9:2]), .currentPC_lower(currentPC[9:2]), .taken(taken), 
-    .taken_predict(output_module_PHT));
+    //update BTB and get nextPC
+    always @(posedge clk)begin
+        if(rst)begin
+            for(i = 0; i < 1024; i = i + 1)begin
+                valid[i] <= 0;
+            end
+        end
+        else begin
+            if(train_valid0)begin
+                BTB[address_branch0[11:2]] <= {address_branch0, address_result0};
+                valid[address_branch0[11:2]] <= isbranch0;
+                if(PC0 == address_branch0)begin
+                    nextPC0 <= isbranch0? address_result0 : PC0 + 4;
+                end
+                else if(PC0 == BTB[PC0[11:2]][63:32]) begin
+                    nextPC0 <= valid[PC0[11:2]] ? BTB[PC0[11:2]][31:0] : PC0 + 4;
+                end
+                else begin
+                    nextPC0 <= PC0 + 4;
+                end
+            end
+            else begin
+                if(PC0 == BTB[PC0[11:2]][63:32]) begin
+                    nextPC0 <= valid[PC0[11:2]] ? BTB[PC0[11:2]][31:0] : PC0 + 4;
+                end
+                else begin
+                    nextPC0 <= PC0 + 4;
+                end
+            end
+            if(train_valid1)begin
+                BTB[address_branch1[11:2]] <= {address_branch1, address_result1};
+                valid[address_branch1[11:2]] <= isbranch1;
+                if(PC1 == address_branch1)begin
+                    nextPC1 <= isbranch1? address_result1 : PC1 + 4;
+                end
+                else if(PC1 == BTB[PC1[11:2]][63:32]) begin
+                    nextPC1 <= valid[PC1[11:2]] ? BTB[PC1[11:2]][31:0] : PC1 + 4;
+                end
+                else begin
+                    nextPC1 <= PC1 + 4;
+                end
+            end
+            else begin
+                if(PC1 == BTB[PC1[11:2]][63:32]) begin
+                    nextPC1 <= valid[PC1[11:2]] ? BTB[PC1[11:2]][31:0] : PC1 + 4;
+                end
+                else begin
+                    nextPC1 <= PC1 + 4;
+                end
+            end
+        end
+    end
     
-    module_BTB_direct module_3(.clk(clk), .rst(rst), .isbranch(output_module_isbranch), 
-    .currentPC(currentPC), .update(update), .branchPC(branchPC), .resultPC(resultPC), 
-    .target(target));
+    assign target0 = taken_predict0 ? nextPC0 : PC0 + 4;
+    assign target1 = taken_predict1 ? nextPC1 : PC1 + 4;
     
 endmodule
